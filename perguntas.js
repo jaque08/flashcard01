7328e96a74fbd666d08d17821feb34bd8a32d995
jaque08 <146119995+jaque08@users.noreply.github.com>
2024-11-19 16:54:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Eufemismo',
+    'O que é eufemismo?',
+    'É quando suavizamos uma frase que tem um tom desagradável sem alterar o sentido original dela.'
+)
+
+criaCartao(
+    'Eufemismo',
+    'Um exemplo de quando usar o eufemismo:',
+    'Podemos usar ele, por exemplo, quando não queremos ofender a pessoa com quem estamos falando.'
+)
+
+criaCartao(
+    'Eufemismo',
+    'Exemplo de eufemismo:',
+    'Ela faltou com a verdade.'
+)
+
+criaCartao(
+    'Eufemismo',
+    'Utilize Eufemismo na frase: ele está pobre.',
+    'Ele está em uma situação financeira delicada.'
+)
